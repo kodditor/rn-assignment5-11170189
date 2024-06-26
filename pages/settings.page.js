ffc72c466d712bc87808092b43050948e5b2e5dc
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-06-26 16:21:41 +0000

    Added settings page

---
+++ pages/settings.page.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+
+export default function SettingsPage({ navigation }) {
+
+    return (
+        <View>
+            <Text>Settings</Text>
+        </View>
+    )
+}
